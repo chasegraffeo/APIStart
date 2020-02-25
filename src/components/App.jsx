f85d1ae288afe97114ca615016274c95c395152f
Chase Graffeo <chasegraffeo@gmail.com>
2020-02-25 13:32:07 -0600

    start gib

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,22 +2,62 @@ import React, { Component } from "react";
 /* import logo from './logo.svg';
 import './App.css';
  */
-import Button from 'react-bootstrap/Button';
-
+import Button from "react-bootstrap/Button";
+//import ProgressBar from "react-bootstrap/ProgressBar";
+// import Button from "react-bootstrap/Button";
+// import Button from "react-bootstrap/Button";
+// import Button from "react-bootstrap/Button";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: 'hello'
+      text: "Studio Ghiblis",
+      movies: [],
+      people: [],
     };
   }
+  componentDidMount() {
+    fetch("https://ghibliapi.herokuapp.com/films")
+      .then(response => response.json())
+      .then(data => this.setState({ movies: data }))
+      .then(console.log(this.state.movies))
+  }
+  
+
+
 
   render() {
-    return <React.Fragment>
-      <h1>{this.state.text}</h1>
-      <Button variant="warning">Warning</Button>{' '}
-    </React.Fragment>;
+    return (
+      <React.Fragment>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "gray"
+          }}
+        >
+          <h1>{this.state.text}</h1> 
+          <br/>
+          <br/>
+          <span>
+            <Button variant="dark" href="#">
+              Link
+            </Button>            
+            <Button variant="dark" href="https://ghibliapi.herokuapp.com/#">
+              Link
+            </Button> 
+            <Button variant="dark" href="#">
+              Link
+            </Button>
+          </span>
+          <br/>
+          {/* <ProgressBar animated now={45} /> */}
+        </div>
+      </React.Fragment>
+    );
   }
 }
 
